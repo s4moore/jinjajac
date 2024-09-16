23b038ead5a2dcec9520487f0990f883bc2cf257
Samuel Moore <samuelallister@outlook.com>
2024-09-16 07:18:35 +0100

    added tags

--- js/input.js
+++ js/input.js
@@ -2,10 +2,10 @@ import { showOverlay, handleClose} from './overlay.js';
 import { changeCollection, currentCollection } from './collection.js';
 import { prevSlide, nextSlide, getNextSlide, slides, currentSlide } from './slides.js';
 import { toggleMenu, openGalleryMenu } from './utils.js';
-import { fadeInButtons, toggleConnect, galleryTransitionEnd } from './utils.js';
+import { fadeInButtons, toggleConnect, toggleAbout } from './utils.js';
 import { fullScreen } from './fullscreen.js';
-import { openGallery, closeGallery, galleryHidden } from './gallery.js';
-import { collections, overlay, updateImages, currentIndex, swipeDown, swipeUp } from '../script.js';
+import { openGallery } from './gallery.js';
+import { collections, overlay } from '../script.js';
 import { moreAbout } from './about.js';
 
 
@@ -16,9 +16,11 @@ export function handleStart(e) {
 	// if (e.target.closest('.menu-toggle')) {
 	// 	return ;
 	// }
-	if (e.target.closest('.gallery-img')) {
-		return ;
-	}
+    const screenWidth = window.innerWidth;
+    const target = e.target;
+    if (target.closest('.gallery img')){
+        return ;
+    }
 	if (e.target.closest('.Connect')) {
 		toggleConnect();
 		return;
@@ -29,7 +31,7 @@ export function handleStart(e) {
     }
     console.log('Start event target:', e.target);
     if (!e.target.closest('.overlay') && !e.target.closest('.overlay-img')
-        && !e.target.closest('.carousel-slide') && !e.target.closest('.scroll-header')) {
+        && !e.target.closest('.carousel-slide')) {
 	if (e.target === '.gallery-button'){
 		e.target = document.querySelector('.gallery-menu');
 	}
@@ -45,48 +47,27 @@ export function handleStart(e) {
         });
     }
     }
-    // if (e.target.tagName.toLowerCase() === 'a' || e.target.closest('a')) {
-    //     return;
-    // }
         startX = e.touches ? e.touches[0].clientX : e.clientX;
         startY = e.touches ? e.touches[0].clientY : e.clientY;
-}
-let collectionsHidden = true;
-
-const galleryOverlay = document.querySelector('.gallery');
-
-export function handleEnd(e) {
-    const target = e.target;
-	if (target.closest('.Connect') || target.closest('.gallery-img')) {
-		return ;
-	}
-	e.preventDefault();
-    console.log('End event target:', target);
-    if (target.closest('.Gallery')) {
+        if (target.closest('.Gallery')) {
             const overlay = document.querySelector('.overlay');
         if (overlay){
             overlay.remove();
             openGallery();
         }   else {
+        document.querySelectorAll('.active').forEach(element => {
+            element.classList.add('hidden');
+            // showOverlay();
+            console.log('going to overlay mode');
+            slides.forEach (slide => slide.classList.add('paused'));
             openGallery();
+
+        });
         }
 		return ;
     }
-	// if (target.closest('.scroll-header')) {
-	// 	updateImages(currentIndex);
-	// }
 	if (target.closest('.lower-button')) {
-		// openGalleryMenu();
-		const openMenu = document.querySelector('.galleriesMenu:not(.hidden)');
-		openMenu.removeEventListener('transitionend', galleryTransitionEnd);
-		openMenu.removeEventListener('animationend', galleryTransitionEnd);
-		openMenu.classList.add('highlight2');
-		openMenu.classList.remove('gallery-fade');
-		openMenu.addEventListener('animationend', () => {
-			openMenu.classList.remove('highlight2');
-			openMenu.classList.add('fadeOut');
-			openMenu.addEventListener('transitionend', galleryTransitionEnd); 
-		});
+		openGalleryMenu();
 		const current = collections[currentCollection].collection;
 		console.log('Current collection:', current);
 		switch (current) {
@@ -94,7 +75,7 @@ export function handleEnd(e) {
 				changeCollection('.Lighting');
 				break;
 			case '.Lighting':
-				changeCollection('.Concrete');
+				changeCollection('.Digital');
 				break;
 			case '.Digital':
 				changeCollection('.Concrete');
@@ -105,25 +86,15 @@ export function handleEnd(e) {
 		return ;
 	}
 	if (target.closest('.upper-button')) {
-		// openGalleryMenu();
+		openGalleryMenu();
 		const current = collections[currentCollection].collection;
 		console.log('Current collection:', current);
-		const openMenu = document.querySelector('.galleriesMenu:not(.hidden)');
-		openMenu.removeEventListener('transitionend', galleryTransitionEnd);
-		openMenu.removeEventListener('animationend', galleryTransitionEnd);
-		openMenu.classList.add('highlight2');
-		openMenu.classList.remove('gallery-fade');
-		openMenu.addEventListener('animationend', () => {
-			openMenu.classList.remove('highlight2');
-			openMenu.classList.add('fadeOut');
-			openMenu.addEventListener('transitionend', galleryTransitionEnd); 
-		});
 		switch (current) {
 			case '.Concrete':
 				changeCollection('.Digital');
 				break;
 			case '.Lighting':
-				changeCollection('.Digital');
+				changeCollection('.Concrete');
 				break;
 			case '.Digital':
 				changeCollection('.Lighting');
@@ -133,7 +104,6 @@ export function handleEnd(e) {
 		}
 		return ;
 	}
-
 	if (target.closest('.Connect')) {
 		toggleConnect();
 		return ;
@@ -143,25 +113,15 @@ export function handleEnd(e) {
 		const emailAddress = 'info@jinjajac.com';
 		const subject = '';
 		const body = '';
-	
 		emailLink.href = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-		emailLink.click();
 	}
 	if (target.closest('.More')) {
-		// toggleAbout();
-		if (!galleryHidden) {
-			closeGallery();
-		}
-		if (document.querySelector('.overlay')) {
-			handleClose();
-		}
 		moreAbout();
 		return ;
 	}
 	if (target.closest('.Instagram')) {
 		const instagramLink = document.getElementById('instagramLink');
 		const instagramURL = 'https://www.instagram.com/jinjajac';
-
 		instagramLink.href = instagramURL;
 		instagramLink.click();
 		return;
@@ -169,56 +129,31 @@ export function handleEnd(e) {
 	if (target.closest('.Whatsapp')) {
 		const whatsappLink = document.getElementById('whatsappLink');
 		const phoneNumber = '027782940371'; 
-		const message = 'You\'re ugly and your mother dresses you funny';
-
+		const message = '';
 		whatsappLink.href = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 		whatsappLink.click();
 		return;
 	}
-	if (target.closest('.gallery-change')) {
+	if (target.closest('.gallery-button')) {
 		console.log('Gallery button clicked');
-		document.querySelector('.galleriesButton:not(.hidden)').classList.add('highlight2');
-		document.querySelector('.galleriesButton:not(.hidden)').addEventListener('animationend', () => {
-			document.querySelector('.galleriesButton:not(.hidden)').classList.remove('highlight2');
-		});
 		openGalleryMenu();
 		return ;
 	}
     if (target.closest('.Concrete')) {
-		closeGallery();
-		toggleMenu();
+        handleClose ();
         changeCollection('.Concrete');
-		handleClose ();
-		// getNextSlide();
-
         return ;
     }
 	if (target.closest('.Lighting')) {
-		closeGallery();
-
-		toggleMenu();
+        handleClose ();
         changeCollection('.Lighting');
-		handleClose ();
-		// getNextSlide();
         return ;
     }
     if (target.closest('.Digital')) {
-		closeGallery();
-
-		toggleMenu();
         changeCollection('.Digital');
-		handleClose ();
-
-		// getNextSlide();
+        handleClose ();
         return ;
     }
-
-
-    const screenWidth = window.innerWidth;
-
-
-    const menu = document.querySelector('.menu');
-
 
     if (target.closest('#change-menu-btn')) {
         console.log('Menu button clicked');
@@ -230,46 +165,50 @@ export function handleEnd(e) {
 
 		handleClose();
 		nextSlide();
-		// resolve();
 		return ;
     }
-	endX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
-    endY = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
-    const xMove = endX - startX;
-    const yMove = endY - startY;
-	if (target.closest('.scroll-header')) {
-		updateImages();
-		// updateImages(currentIndex);
-		if (Math.abs(yMove) > 10) {
-            if (Math.abs(yMove) > 10) {
-				
-                if (startY > endY + 10) {
-					swipeDown();
-                    return ;
-                } else if (startY < endY - 10) {
-                    console.log('Swiped down');
-					swipeUp();
-                    return ;
-                }
-            }
-		}
-	}
     if (target.closest('.fullscreen-button')) {
         fullScreen();
         return ;
     }
-    if (galleryHidden && startX <= screenWidth * 0.2) {
+}
+
+
+export function handleEnd(e) {
+    const target = e.target;
+	// if (target.closest('.links')) {
+	// 	setTimeout(toggleMenu, 1000);
+	// 	return ;
+	// }
+    if (target.closest('.gallery img, img')){
+        return ;
+    }
+	if (target.closest('.Connect')) {
+		return ;
+	}
+	e.preventDefault();
+    const screenWidth = window.innerWidth;
+
+    console.log('End event target:', target);
+	const anchor = target.closest('a');
+    // if (anchor) {
+	// 		setTimeout(toggleMenu, 1000);
+    //     // return;
+    // }
+    if (startX <= screenWidth * 0.2) {
         prevSlide();
         return;
-    } else if (galleryHidden && startX >= screenWidth * 0.8 )  {
+    } else if (startX >= screenWidth * 0.8 )  {
         getNextSlide();
         return;
     }
 
+    endX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
+    endY = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
+    const xMove = endX - startX;
+    const yMove = endY - startY;
 
-
-
-	if (Math.abs(xMove) > 10 || Math.abs(yMove) > 10) {
+    if (Math.abs(xMove) > 10 || Math.abs(yMove) > 10) {
         if (Math.abs (xMove) > Math.abs(yMove)) {
             if (startX > endX + 10) {
                 getNextSlide();
@@ -278,7 +217,19 @@ export function handleEnd(e) {
                 prevSlide();
                 return ;
             }
-	}
+        // } else if (Math.abs(yMove) > 10) {
+        //     if (!document.querySelector('.overlay') && Math.abs(yMove) > 10) {
+        //         if (startY > endY + 10) {
+        //             console.log('Swiped up');
+        //             changeCollection(1);
+        //             return ;
+        //         } else if (startY < endY - 10) {
+        //             console.log('Swiped down');
+        //             changeCollection(-1);
+        //             return ;
+        //         }
+        //     }
+        } 
     } else if (!document.querySelector('.overlay')) {
         document.querySelectorAll('.active').forEach(element => {
             element.classList.add('hidden');
